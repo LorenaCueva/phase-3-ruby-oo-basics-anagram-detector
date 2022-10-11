112ef8293c719cd4f2ad27724f78d49654ad7d9d
Lorena Cueva <lorenacueva@gmail.com>
2022-10-11 14:45:22 -0700

    commit tree e26263ca6f5b5bbc19932f2e84d811d8ef768724

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def sort_word(word)
+        word.downcase.chars.sort
+    end
+
+
+    def match(arr)
+        word_to_match = sort_word(@word)
+        arr.select{|w| sort_word(w) == word_to_match}
+    end
+end
